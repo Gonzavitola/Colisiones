24f5cd9a25262f2c204b797b3f0086ec1ca04e4b
Gonzavitola <101563291+Gonzavitola@users.noreply.github.com>
2022-05-24 15:55:37 -0300

    FINISH

---
+++ Assets/Playercolision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercolision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y<0)
+        {
+            transform.position = new Vector3(3.34f, 0.5f, 3.28f);
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name=="deadwall")
+        {
+            transform.position = new Vector3 (3.34f, 0.5f, 3.28f);
+        }
+       
+    }
+}
